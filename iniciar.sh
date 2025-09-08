fa57da3a6a3734cc1d14dea6d6f8af7cd732284a
luckasklopes <lkmrlucas@gmail.com>
2025-09-08 16:07:17 -0500

    integração inicial da pipeline de deploy #8

--- iniciar.sh
+++ iniciar.sh
@@ -2,14 +2,12 @@
 # Só deve ser executado quando o arquivo .env estiver criado e configurado corretamente
 
 # WORK IN PROGRESS
+cp .env.example .env
 
 docker-compose up -d --build
 
 docker exec -it laravel_php composer install
 docker exec -it laravel_php npm install && npm run build
 
-cp .env.example .env
-
 docker exec -it laravel_php php artisan key:generate
-
 docker exec -it laravel_php php artisan migrate

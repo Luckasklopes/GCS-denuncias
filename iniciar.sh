b094c2dde5531e43de33a49eae7488f3d3eae720
luckasklopes <lkmrlucas@gmail.com>
2025-09-09 14:05:03 -0500

    correção de comandos docker exec para garantir funcionamento do scrip de ativação

--- iniciar.sh
+++ iniciar.sh
@@ -8,8 +8,8 @@ cp .env.example .env
 
 docker compose up -d --build
 
-docker exec -it denuncias-app composer install
-docker exec -it denuncias-app npm install && npm run build
+docker exec denuncias-app composer install
+docker exec denuncias-app npm install && npm run build
 
-docker exec -it denuncias-app php artisan key:generate
-docker exec -it denuncias-app artisan migrate
+docker exec denuncias-app php artisan key:generate
+docker exec denuncias-app artisan migrate

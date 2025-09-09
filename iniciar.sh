aad4413bae7a8d34f7236eae4da45095953ae65f
Lucas Lopes <125325077+Luckasklopes@users.noreply.github.com>
2025-09-09 11:13:24 -0500

    correções script para pipeline #6

--- iniciar.sh
+++ iniciar.sh
@@ -8,8 +8,8 @@ cp .env.example .env
 
 docker compose up -d --build
 
-docker exec -it gcs-denuncias-app composer install
-docker exec -it gcs-denuncias-app npm install && npm run build
+docker exec -it denuncias-app composer install
+docker exec -it denuncias-app npm install && npm run build
 
-docker exec -it gcs-denuncias-app php artisan key:generate
-docker exec -it gcs-denuncias-app artisan migrate
+docker exec -it denuncias-app php artisan key:generate
+docker exec -it denuncias-app artisan migrate

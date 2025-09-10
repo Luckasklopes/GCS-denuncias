da485104bd20802cb7b9799c0ca8fb0f44975e1a
luckasklopes <lkmrlucas@gmail.com>
2025-09-10 09:46:24 -0500

    correção scrip inicialização nº998 #6

--- iniciar.sh
+++ iniciar.sh
@@ -13,5 +13,17 @@ docker exec denuncias-app composer install
 docker exec denuncias-app npm install
 docker exec denuncias-app npm run build
 
+# Espera o MySQL estar pronto
+until docker exec denuncias-banco mysqladmin ping -h "127.0.0.1" --silent; do
+  echo "⏳ Aguardando o banco ficar pronto..."
+  sleep 2
+done
+
+# Agora roda as migrations
+docker exec denuncias-app php artisan migrate
+
+
 docker exec denuncias-app php artisan key:generate
 docker exec denuncias-app php artisan migrate
+
+echo "FUNCIONOU"
